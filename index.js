24603f76acecbb66eb44efe0fb90e486e7a06b77
pauli <259199@student.pwr.edu.pl>
2023-07-08 21:25:17 +0200

    saving data to CSV file, adding some corrections (lists are now merged, way of dealing with duplicates is changes slightly, a more asynchronous approach added)

--- index.js
+++ index.js
@@ -3,24 +3,27 @@ const PORT = 8000
 const express = require('express')
 const cheerio = require('cheerio')
 const axios = require('axios')
+const createCsvWriter = require('csv-writer').createObjectCsvWriter
 
 const app = express()
 
-// Base url of the webpage for scraping
+// Base URL of the webpage for scraping
 const baseURL = 'https://www.filmweb.pl'
 
-// Url of the 'initial' page HTTP request is sent to
+// URL of the 'initial' page HTTP request is sent to
 const url = `${baseURL}/ranking/vod/film`
 
 // Array of VOD platforms
-const listOfPlatforms = []
+const vodPlatformURLs = []
 
+// Actual year
 const actualYear = new Date().getFullYear()
 
-// Sending a HTTP request via axios package
-axios(url)
-    .then(response => {
-        // If the promise is resolved -> code of the callback function will be executed
+const scrapedMovies = async () => {
+    try {
+        // Sending an HTTP request via axios package
+        // scrapedMovies function is waiting for the HTTP request to given url to be completed and for the response from the server
+        const response = await axios(url)
 
         // Saving received response as an HTML
         const html = response.data
@@ -42,123 +45,112 @@ axios(url)
                 const vodURL = `${baseURL}${vodHref}/${actualYear}`
 
                 // Merged URL is pushed to the array of VOD Platforms
-                listOfPlatforms.push(vodURL)
+                vodPlatformURLs.push(vodURL)
             }
         })
-        console.log(listOfPlatforms)
 
-        // Sending HTTP requests to individual VOD URL
-        const requests = listOfPlatforms.map(vodURL => axios(vodURL))
+        // Sending HTTP requests to individual VOD URLs
+        const requests = vodPlatformURLs.map(vodURL => axios(vodURL))
 
-        // Wait for the first request to be completed, send next requests when the vodPlatforms array is already filled with data
-        Promise.all(requests)
-            .then(responses => {
-                // Final rankings of movies with duplicates removed & sorted by rating in descending order
-                const vodPlatforms = {
-                    netflix: [],
-                    hbo_max: [],
-                    canal_plus_manual: [],
-                    disney: [],
-                }
+        // Wait for all requests to be completed, send next requests when the vodPlatformURLs array is already filled with data
+        const responses = await Promise.all(requests)
 
-                responses.forEach(response => {
-                    const html = response.data
-                    const $ = cheerio.load(html)
+        // Array of all movies scraped
+        let allMovies = []
 
-                    // Array to store all scraped movies
-                    const allMovies = []
+        const allMoviesPromises = responses.map(async response => {
+            const html = response.data
+            const $ = cheerio.load(html)
 
-                    // Scraping movies from VOD platforms
-                    $('.rankingType', html).each(function () {
-                        const title = $(this).find('.rankingType__title').text()
+            // Scraping movies from VOD platforms
+            const movies = $('.rankingType', html)
+                .slice(0, 10) // Limit to max. 10 movies per one platform
+                .map(function () {
+                    const title = $(this).find('.rankingType__title').text()
 
-                        // String to float conversion (ex.: '8,5' -> '8.5') so they're comparable value
-                        const stringRating = $(this)
-                            .find('.rankingType__rate--value')
-                            .text()
-                        const normalizedStringRating = stringRating.replace(',', '.')
-                        const rating = parseFloat(normalizedStringRating)
-
-                        allMovies.push({
-                            title,
-                            rating,
-                        })
-                    })
-
-                    // Adding movies to the specific VOD platform's ranking
                     const platform = response.config.url
                         .replace(baseURL, '')
                         .replace('/ranking/vod/', '')
                         .replace(`/film/${actualYear}`, '')
 
-                    vodPlatforms[platform] = allMovies
+                    // String to float conversion (ex.: '8,5' -> '8.5') so they're comparable value
+                    const stringRating = $(this).find('.rankingType__rate--value').text()
+                    const normalizedStringRating = stringRating.replace(',', '.')
+                    const rating = parseFloat(normalizedStringRating)
+
+                    return {
+                        title,
+                        platform,
+                        rating,
+                    }
                 })
+                .get()
 
-                // Removing duplicates from the specific platform's list
-                // Iterate over each platform from vodPlatforms
-                for (const platform in vodPlatforms) {
-                    // Retrieve the list of movies of the current platform
-                    const listOfMovies = vodPlatforms[platform]
+            allMovies = allMovies.concat(movies)
+        })
 
-                    // Make sure only top 10 movies are kept by removing extra ones
-                    listOfMovies.splice(10)
+        // Waiting for all movie scraping promises to be completed
+        await Promise.all(allMoviesPromises)
 
-                    // Check if a movie already exists on other platform's list
-                    // If yes, only the higher-rated one stays on the list
-                    listOfMovies.forEach(movie => {
-                        for (const otherPlatform in vodPlatforms) {
-                            // Make sure it's a different platform
-                            if (otherPlatform !== platform) {
-                                // Retrieve the list of movies of the other platform
-                                const otherMovies = vodPlatforms[otherPlatform]
-                                // Find a movie with a matching title
-                                const existingMovie = otherMovies.find(
-                                    m => m.title === movie.title
-                                )
+        // Merging separate arrays (each for one VOD platform) into one array
+        const mergedMovies = [].concat(...allMovies)
 
-                                // Compare their ratings
-                                if (existingMovie && existingMovie.rating < movie.rating) {
-                                    // Remove a movie with lower rating
-                                    otherMovies.splice(otherMovies.indexOf(existingMovie), 1)
-                                    // Add a movie from the current platform's list with the same title
-                                    const replacementMovie = listOfMovies.find(
-                                        m => m.title === movie.title
-                                    )
-                                    if (replacementMovie) {
-                                        otherMovies.push(replacementMovie)
-                                    }
-                                }
-                            }
-                        }
-                    })
-
-                    // Add movies from the same platform if the list has less than 10 movies
-                    while (listOfMovies.length < 10) {
-                        const remainingMovies = listOfMovies.length - 1 // Number of missing movies
-                        const additionalMovies = vodPlatforms[platform].slice(
-                            10,
-                            10 + remainingMovies
-                        )
-                        listOfMovies.push(...additionalMovies)
-
-                        // Exit the loop if there's no more additional movies available
-                        if (additionalMovies.length === 0) {
-                            break
-                        }
-                    }
-
-                    // Limit the list to less than 10 if necessary
-                    if (listOfMovies.length > 10) {
-                        listOfMovies.splice(10)
-                    }
-
-                    // Sorting the list of movies in descending order
-                    listOfMovies.sort((a, b) => b.rating - a.rating)
+        // Removing duplicates from the final list of movies
+        // listMovie stores an 'unique' not duplicated movie
+        // currentMovie is currently processed movie
+        const listOfMovies = mergedMovies.reduce((listMovie, currentMovie) => {
+            // Find a movie with a matching title (in order to detect duplicates)
+            const i = listMovie.findIndex(m => m.title === currentMovie.title)
+            // if the movie is not a duplicate add it to the final list
+            if (i === -1) {
+                listMovie.push(currentMovie)
+            } else {
+                // Compare their ratings
+                if (currentMovie.rating > listMovie[i].rating) {
+                    // Keep the higher rating for this title
+                    listMovie[i].rating = currentMovie.rating
                 }
+            }
+            return listMovie // Final list of movies
+        }, [])
 
-                console.log(vodPlatforms)
-            }).catch(err => console.log(err))
-    }).catch(err => console.log(err)) //if the promise is rejected -> callback function is executed with the error information
+        // Sorting the list of movies by rating in descending order
+        listOfMovies.sort((a, b) => b.rating - a.rating)
 
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+        // Saving data to CSV file
+        const csvWriter = createCsvWriter({
+            path: 'web-scraper-results.csv',
+            header: [
+                { id: 'title', title: 'Title' },
+                { id: 'platform', title: 'VOD service name' },
+                { id: 'rating', title: 'Rating' },
+            ],
+        })
+
+        // Replace the platform names with some display names
+        const vodPlatformDisplayNames = {
+            netflix: 'Netflix',
+            hbo_max: 'HBO MAX',
+            canal_plus_manual: 'Canal+',
+            disney: 'Disney+',
+        }
+
+        const records = listOfMovies.map(movie => ({
+            title: movie.title,
+            platform: vodPlatformDisplayNames[movie.platform] || movie.platform, // Default name
+            rating: movie.rating,
+        }))
+
+        // Waiting for CSV file to be filled with records, then logs a message to informing about the success/error
+        await csvWriter.writeRecords(records)
+        console.log('Data successfully written to CSV file')
+    } catch (error) {
+        console.log('An error occurred: ', error)
+    }
+}
+
 //callback function - if this is working it says on which port the server is running
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`)
+    scrapedMovies()
+})
